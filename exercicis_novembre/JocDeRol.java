a976e00f8849fdc45f9e48ec73229310daae9dbb
sebasprofe17 <sebasprofe17@gmail.com>
2020-11-20 20:41:18 +0100

    En la tirada ja feta

--- exercicis_novembre/JocDeRol.java
+++ exercicis_novembre/JocDeRol.java
@@ -18,11 +18,36 @@ public class JocDeRol{
         //DEmanar cantitat valida de usuaris
         System.out.println("Quants jugadors van a jugar.");
         cantitat_jugadors = demanarCantitat(MINIM_JUGADORS, MAXIM_JUGADORS);
+        usuaris = new String[cantitat_jugadors];
+        jugadors = new char[cantitat_jugadors];
+        vida = new int[cantitat_jugadors];
+
+        inicialitza(vida, 30);
+    
+        /*
         //Provar
         System.out.println("Has introuit el "+cantitat_jugadors);
         //Demanar la tirada i comprovar que es correcta
-        
+        System.out.println("Quina tirada fas.");
+        dado = demanarCantitat(1,6);
+        //Provar
+        System.out.println("Has introuit el "+dado);
+        */
     }
+    
+    public static void inicialitza(int[] v, int valor){
+        for(int i = 0; i<v.length; i++)
+            v[i] = valor;
+    }
+    
+    /**
+    demanarNoms
+    input, el array de usuaris
+    ouput res
+    ha de demanar i posar en cada posicio el nom de lusuari
+    */
+    
+    
     /**
     Este metode torna un numero valid, entre un minim i maxim
     */
@@ -39,16 +64,15 @@ public class JocDeRol{
     }
     
     public static boolean comprovarCantitat(int minim, int maxim, int numero){
-        if(numero <= minim || numero >= maxim){
+        if(numero < minim || numero > maxim){
             return false;
         }
         return true;
     }
     
     /**
-    int demanarTirada
-    int demanarCantitat
-    boolean comprovarCantitat
+    Pintar vida
+    Input,el array en , el array amb la vida dels jugadors
     */
-    
+
 }
